0ecf70ded3308785fedc3e93730222863ce3037d
Yash Soni <yashjpsoni264@gmail.com>
2020-07-12 18:00:00 +0530

    Configuration file

---
+++ Temperature Monitoring System/conf.py
@@ -0,0 +1,6 @@
+"""Configuration file for Temperatur alert System"""
+bolt_api_key = "here your bolt api key without space (use the '-' provided in the key, and insert the key in "")"
+device_id = "your bolt device id"
+telegram_chat_id = "@your_telegram_chat_id"
+telegram_bot_id = "botxxyxxy:bot_id"
+threshold = 250
