d6c15cfc5ad5278b9461f291ad142a6e0d4e8bb5
Yash Soni <yashjpsoni264@gmail.com>
2020-07-12 18:50:27 +0530

    Configuration File

--- Anamoly Detection/conf.py
+++ Anamoly Detection/conf.py
@@ -1,3 +1,12 @@
 """Configuration file for the Anomaly Detection Project """
 
-API_KEY = "
+bolt_api_key = "here your bolt api key without space (use the '-' provided in the key, and insert the key in "")"
+device_id = "your bolt device id"
+telegram_chat_id = "@your_telegram_chat_id"
+telegram_bot_id = "botxxyxxy:bot_id"
+SID = "XXXXXXXXXXXXXX09ce"
+AUTH_TOKEN = "XXXXXXXXXXXXXXXXXXXXX70"
+TO_NUMBER = "94xxxxxx49"
+FROM_NUMBER = "+12xxxxxx21"
+Frame_size = 10
+Mul_factor = 3
